dc00a367879c62b36758672f0dd00e4ed54f609c
Shamim Ahmed <shamimahmed30082@gmail.com>
2022-09-07 01:34:49 +0600

    created profile app

--- mainProject/firstProject/urls.py
+++ mainProject/firstProject/urls.py
@@ -2,6 +2,10 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from users import views as user_views
 
 urlpatterns = [
@@ -16,3 +20,7 @@ urlpatterns = [
     path('profile/', include('users.urls')),
 
 ]
+
+if settings.DEBUG:
+
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
